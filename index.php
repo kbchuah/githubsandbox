476dc095bfd1c6e9c4ea0a7668f2b45c02260d64
kohmander <chuahkohbeng@gmail.com>
2018-08-28 15:56:41 +0800

    Edited

--- index.php
+++ index.php
@@ -5,11 +5,7 @@
 <html>
   <body>
     <h1>
-<<<<<<< HEAD
-      Hello World Test KB 456
-=======
-      Hello World Test KB 123
->>>>>>> 2251b2dab4344e61a6f4186eea35cd928951ad89
+      Hello World Test KB 123 456
     </h1>
   </body>
 </html>
